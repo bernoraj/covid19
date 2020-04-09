1f8b1e336c23feb56bdb5a0ef87ee44323537f1e
bernoraj <bernomay20@gmail.com>
2020-04-10 00:35:00 +0530

    3rd

--- src/app/dashboard.service.ts
+++ src/app/dashboard.service.ts
@@ -15,5 +15,13 @@ export class DashboardService {
     .pipe(map((res: Response) => res.json()));
   }
 
+  getTNStats(){
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });    
+    return this.http.get('/api/v1/covid_TN', options )
+    .pipe(map((res: Response) => res.json()));
+  }
+
+
   constructor(private http: Http) { }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,29 +16,86 @@ export class DashboardComponent implements OnInit {
   sortByRecovered;
   tempData;
   lastcheck;
+  //final
+  maleCount;
+  femaleCount;
+  recoveredCount;
+  confirmedCount;
+  deceasedCount;
+  hospitalizedCount;
   constructor(private dashboardService:DashboardService) { }
 
-  ngOnInit(): void {   
-    this.dashboardService.getStats().subscribe(data=>{      
-      this.tempData=data.data.covid19Stats;     
-      this.lastcheck=new Date(data.data.lastChecked).toLocaleDateString();
-   var test= _.chain(this.tempData).groupBy('country')
-     .map(g => {
-      return {
-        name: g[0].country,
-        confirmed: _.sumBy(g, 'confirmed'), 
-        deaths: _.sumBy(g, 'deaths'),
-        recovered:_.sumBy(g,'recovered')
+  ngOnInit(): void {  
+    
+    
+  //   this.dashboardService.getStats().subscribe(data=>{      
+  //     this.tempData=data.data.covid19Stats;     
+  //     this.lastcheck=new Date(data.data.lastChecked).toLocaleDateString();
+  //  var test= _.chain(this.tempData).groupBy('country')
+  //    .map(g => {
+  //     return {
+  //       name: g[0].country,
+  //       confirmed: _.sumBy(g, 'confirmed'), 
+  //       deaths: _.sumBy(g, 'deaths'),
+  //       recovered:_.sumBy(g,'recovered')
+  //     }
+  //   }).value();
+  //   this.covidData=_.slice(test,0,3);    
+  //   this.sortByConfirmed=_.orderBy(test, ['confirmed'],['desc']).slice(0,3);    
+  //   this.sortByDeaths=_.orderBy(test, ['deaths'],['desc']).slice(0,3);
+  //   this.sortByRecovered=_.orderBy(test, ['recovered'],['desc']).slice(0,3);
+    
+  //   });
+   
+      
+  this.dashboardService.getTNStats().subscribe(data=>{      
+    this.tempData=data;
+
+    //Gender Count     
+    var countbyGender=_.countBy(this.tempData,{'gender':'M'});
+    this.maleCount=countbyGender.true;
+    this.femaleCount=countbyGender.false;
+
+    //Recovered,Deceased,Hospitalized Count
+    this.recoveredCount=_.countBy(this.tempData,{'currentstatus':'Recovered'}).true;
+    this.deceasedCount=_.countBy(this.tempData,{'currentstatus':'Deceased'}).true;
+    this.hospitalizedCount=_.countBy(this.tempData,{'currentstatus':'Hospitalized'}).true;
+
+    //District-Wise 
+    var districtData=_.chain(this.tempData).groupBy('detecteddistrict')
+    .map(g=>{
+      return{
+        district:g[0].detecteddistrict,
+        city:_.uniqBy(g,'detectedcity').map(x=>x.detectedcity).filter(t=>t!="" && t!=g[0].detecteddistrict),        
+        hospitalized:g.map(y=>y.currentstatus).filter(y=>y=='Hospitalized').length,        
+        deceased:g.map(y=>y.currentstatus).filter(y=>y=='Deceased').length,   
+        recovered:g.map(y=>y.currentstatus).filter(y=>y=='Recovered').length,
+        furter:g               
       }
     }).value();
-    this.covidData=test;
-    this.sortByConfirmed=_.orderBy(test, ['confirmed'],['desc']);    
-    this.sortByDeaths=_.orderBy(test, ['deaths'],['desc']);
-    this.sortByRecovered=_.orderBy(test, ['recovered'],['desc']);
-    
-    });
+
+    //Date-Wise
+    var dateWise=_.chain(this.tempData).groupBy('dateannounced')
+    .map(d=>{
+      return{
+        date:d[0].dateannounced
+
+      }
+    })
+
+    // console.log('distHosp',districtData.reduce(function(acc,dd){ return acc+dd.hospitalized},0));
+    // console.log('distDec',districtData.reduce(function(acc,dd){ return acc+dd.deceased},0));
+    // console.log('distRec',districtData.reduce(function(acc,dd){ return acc+dd.recovered},0));
+
+    console.log('Malecount',this.maleCount);
+    console.log('femaleCount',this.femaleCount);
+    console.log('recoveredCount',this.recoveredCount);
+    console.log('deceasedCount',this.deceasedCount);
+    console.log('hospitalizedCount',this.hospitalizedCount);
+    console.log('District Data',districtData);
    
-   
+  
+  });
     
   
   } 
